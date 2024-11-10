91807f562be5577e62f0a0b69c876e3f3d7db7e0
Dilumuene Kiaku <chimiefriends@gmail.com>
2024-11-10 01:30:41 +0100

    Podomoro
    Podomoro chronomètre

---
+++ podomoro/app.js
@@ -0,0 +1,46 @@
+const start = document.getElementById('start');
+const stop = document.getElementById('stop');
+const reset = document.getElementById('reset');
+const timer = document.getElementById('timer');
+
+let timeLeft = 60;
+let interval;
+
+const updateTimer = () => {
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+
+    timer.innerHTML = 
+    `${minutes.toString().padStart(2,"0")}
+    :
+    ${seconds.toString().padStart(2, "0")}`;
+};
+
+const startTimer = () => {
+    interval = setInterval(() => {
+        timeLeft--;
+        updateTimer();
+        if(timeLeft === 0){
+            clearInterval(interval);
+            alert("Time's up!");
+            timeLeft = 60;
+            updateTimer();
+        }
+    }, 
+    1000)
+};
+
+const stopTimer = () => clearInterval(interval);
+
+const resetTimer = () => {
+    clearInterval(interval);
+    timeLeft = 60;
+    updateTimer();
+};
+
+start.addEventListener("click", startTimer);
+stop.addEventListener("click", stopTimer);
+reset.addEventListener("click", resetTimer);
+
+
+
