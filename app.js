c5b6f29a56d7eb98258004defe319d2309be3a57
chimie dilumuene kiaku <chimiefriends@gmail.com>
2025-10-27 02:28:16 +0100

    Actualisation partie 2 js

---
+++ app.js
@@ -0,0 +1,25 @@
+/* Global variables */
+let SCORE;
+let RAND_QUES;
+let NUMS;
+
+/**********************************************/
+
+const scoreBoard = document.querySelector('.score-board');
+const scoreVal = document.querySelector('#score');
+const questionBox = document.querySelector('.question-box');
+const optionBox = document.querySelector('.option-wrapper');
+const playBtn = document.querySelector('.playbtn');
+const exitBtn = document.querySelector('.exit-btn');
+
+/**********************************************/
+
+const optionsBtns = document.querySelectorAll('.option-text'); //for all 4 option's text
+
+
+const optionsSelection = document.querySelectorAll('#opt-btn')
+/**********************************************/
+
+
+
+/**********************************************/
